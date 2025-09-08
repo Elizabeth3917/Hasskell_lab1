a80688a8bbe5f6c7b317b97c82b5b3d0da1f27bf
Yelyzaveta <kravchenkoliza39@gmail.com>
2025-09-08 12:34:50 +0300

    Update Main.hs
    Додала коментарі при виводі

--- Main.hs
+++ Main.hs
@@ -37,11 +37,26 @@ fib n = fib (n-1) + fib (n-2)
 
 main :: IO ()
 main = do
+    putStrLn "Подвоєння парних чисел"
     print (doubleEven [1,2,3,4])
+    
+    putStrLn "Сума непарних чисел"
     print (sumOdd [1,2,3,4,5])
+    
+    putStrLn "Абсолютні значення"
     print (absList [-3,5,-7])
+    
+    putStrLn "Рекурсивна довжина списку"
     print (myLength [1,2,3,4])
+    
+    putStrLn "Рекурсивне перевертання списку"
     print (myReverse [1,2,3])
+    
+    putStrLn "Максимум списку"
     print (myMaximum [3,7,2,9,4])
+    
+    putStrLn "Генерація піфагорових трійок"
     print (take 5 pythagoreanTriples)
+    
+    putStrLn "Числа Фібоначчі"
     print (fib 10)
