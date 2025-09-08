e3c4cc224e9bb41e56c1f2800e1ce34a962bf89a
Yelyzaveta <kravchenkoliza39@gmail.com>
2025-09-08 12:10:38 +0300

    Update Main.hs
    Додала умови

--- Main.hs
+++ Main.hs
@@ -1,35 +1,35 @@
--- #1
+-- #1 Подвоєння парних чисел
 doubleEven :: [Int] -> [Int]
 doubleEven xs = [if even x then 2 * x else x | x <- xs]
 
--- #2
+-- #2 Сума непарних чисел
 sumOdd :: [Int] -> Int
 sumOdd xs = sum[x | x <- xs, odd x]
 
--- #3
+-- #3 Абсолютні значення
 absList :: [Int] -> [Int]
 absList xs = [if x < 0 then -x else x | x <- xs]
 
--- #4
+-- #4 Рекурсивна довжина списку
 myLength :: [a] -> Int
 myLength [] = 0
 myLength (_:xs) = 1 + myLength xs
 
--- #5
+-- #5 Рекурсивне перевертання списку
 myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse (x:xs) = myReverse xs ++ [x]
 
--- #6
+-- #6 Максимум списку
 myMaximum :: Ord a => [a] -> a
 myMaximum [x] = x
 myMaximum (x:xs) = max x (myMaximum xs)
 
--- #7
+-- #7 Генерація піфагорових трійок
 pythagoreanTriples :: [(Int, Int, Int)]
 pythagoreanTriples = [(a,b,c) | a <- [1..20], b <- [1..20], c <- [1..20], a^2 + b^2 == c^2]
 
--- #8
+-- #8 Числа Фібоначчі
 fib :: Int -> Int
 fib 0 = 0
 fib 1 = 1
